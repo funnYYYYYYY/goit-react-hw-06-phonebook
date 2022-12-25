f1a5f9ed59c36dc77bcb82b608ce55b2a05a7154
Pavlo <pasam9300@gmail.com>
2022-12-25 13:39:25 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ import { Provider } from 'react-redux';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
-      </PersistGate> */}
-      <App />
+      </PersistGate>
+      {/* <App /> */}
     </Provider>
   </React.StrictMode>
 );
